f9fcd4c75a527065b2ffb70da55b3146040d51eb
qdi5 <wuzhe_@aliyun.com>
2020-07-12 10:42:47 +0800

    diy小球动画开始

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -66,7 +66,6 @@ export default {
   data () {
     return {
       isShowShopCart: false,
-      // 原始小球数组
       balls: [
         {
           show: false
@@ -87,8 +86,7 @@ export default {
     }
   },
   created () {
-    // 已经下落的小球
-    this.dropBalls = []
+
   },
   methods: {
     sum () {
@@ -96,42 +94,17 @@ export default {
     },
     // 小球下落的方法
     drop (el) {
-      console.log('调用shopcart的drop方法', el)
-      // 从balls里面找到一个隐藏的小球，并放入dropBall里
-      for (let i = 0; i < this.balls.length; i++) {
-        let ball = this.balls[i]
-        if (!ball.show) {
-          ball.show = true
-          ball.el = el
-          this.dropBalls.push(ball)
-          return
-        }
-      }
+    // 找出一个小球，并将它显示
+
     },
     beforeDrop (el) {
-      const ball = this.dropBalls[this.dropBalls.length - 1]
-      const rect = ball.el.getBoundingClientRect()
-      const x = rect.left - 32
-      const y = -(window.innerHeight - rect.top - 22)
-      el.style.display = ''
-      el.style.transform = `translate3d(0,${y}px,0)`
-      const inner = el.getElementsByClassName('inner-hook')[0]
-      inner.style.transform = `translate3d(${x}px,0,0)`
+    
     },
     dropping (el, done) {
-      // 触发页面重绘，作用是？？？
-      this._reflow = document.body.offsetHeight
-      el.style.transform = `translate3d(0,0,0)`
-      const inner = el.getElementsByClassName('inner-hook')[0]
-      inner.style.transform = `translate3d(0,0,0)`
-      el.addEventListener('transitionend', done)
+     
     },
     afterDrop (el) {
-      const ball = this.dropBalls.shift()
-      if (ball) {
-        ball.show = false
-        el.style.display = 'none'
-      }
+      
     }
   },
   computed: {
@@ -251,17 +224,5 @@ export default {
     color: #ffffff
     background-color: rgb(240, 20, 20)
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.4)
-.ball-container
-  .ball
-    position: fixed
-    left: 32px
-    bottom: 22px
-    z-index: 200
-    transition: all .4s cubic-bezier(.49,-.29,.75,.41)
-    .inner
-      width: 16px
-      height: 16px
-      border-radius: 50%
-      background: rgb(0,100,220)
-      transition: all .4s linear
+
 </style>
